f25a669a7b525c9db9e6dadd6ebaec8f5023ecc4
aletsiaaa <angel.valladares2027@frech.superate.org>
2026-07-23 08:48:38 -0600

    Creación Form PHP registro

---
+++ registro.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FeedTogether</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- CSS propio -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <h1 class="text-center mt-5">
+        ¡Bienvenido a FeedTogether!
+    </h1>
+
+    <div class="mb-3">
+        <label for="" class="form-label">Email</label>
+        <input
+            type="email"
+            class="form-control"
+            name=""
+            id=""
+            aria-describedby="emailHelpId"
+            placeholder="abc@mail.com"
+        />
+        <small id="emailHelpId" class="form-text text-body-secondary"
+            >Help text</small
+        >
+    </div>
+    
+
+
+
+
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- JavaScript propio -->
+    <script src="javascript/script.js"></script>
+
+</body>
+
+</html>
